1d4ac216cf9ac9c026328f7fa0de16490c623724
JaraMohnson <jaramohnson@gmail.com>
2022-08-01 16:38:00 -0400

    pushing

--- country.js
+++ country.js
@@ -6,24 +6,60 @@ class Country {
         this.colors = colors;
     }
 }
-let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"]);
+let usa = new Country("USA", "English", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"]);
 let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
-let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
+let germany = new Country("Germany", "German", "Hallo Welt", ["black", "yellow", "red"])
+let france = new Country("France", "French", "Salut tout le monde", ["red", "blue", "white"]);
+let brazil = new Country("Brazil", "Portuguese", "Olá Mundo!", ["green", "blue", "yellow"]);
+let india = new Country("India", "Hindi", "Namaste Duniya!", ["orange", "white", "green"]);
 
 
 function SwitchCountry() {
-    let input = "USA";
-    let country;
+    let input = document.getElementById("CountryList");
 
-    if (input === "USA") {
-        //set country to usa 
-        country = usa;
+    if (input.value === "USA") {    
+        DisplayCountry(usa)
     }
-    else if (input === "Mexico") {
-        country = mexico;
+    else if (input.value === "Mexico") {
+        DisplayCountry(mexico);
     }
-    else if (input === "Algeria") {
-        country = algeria;
+    else if (input.value === "Germany") {
+        DisplayCountry(germany);
     }
-
+    else if (input.value === "France") {
+        DisplayCountry(france);
+    }
+    else if (input.value === "Brazil") {
+        DisplayCountry(brazil);
+    }
+    else if (input.value === "India") {
+        DisplayCountry(india);
+    }
 }
+    function DisplayCountry(Country)
+    {
+        let colors = Country.colors;
+
+        let color1 = document.querySelector("#Color1"); 
+        color1.style.backgroundColor = colors[0]; 
+
+        let color2 = document.querySelector("#Color2");
+        color2.style.backgroundColor = colors[1];
+
+        let color3 = document.querySelector("#Color3");
+        color3.style.backgroundColor = colors[2]; 
+
+        let countryName = document.querySelector("#CountryName");
+        countryName.innerText = Country.name; 
+
+        let officialLanguage = document.querySelector("#OfficialLanguage");
+        officialLanguage.innerText = Country.lang;
+
+        let helloWorld = document.querySelector("#HelloWorld");
+        helloWorld.innerText = Country.greeting;
+
+        // let countryFlag = document.querySelector("#CountryFlag");
+        // countryFlag.setAttribute("src", "https://i.kym-cdn.com/entries/icons/mobile/000/030/157/womanyellingcat.jpg");
+    }
+
+    
